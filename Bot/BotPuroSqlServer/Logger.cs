2f37fb121804c5e9bc167691cac13627351afbe3
Rekjel07 <sanchezabj07@keemail.me>
2021-02-03 14:32:56 -0600

    Cambios realizados en servidor

---
+++ Bot/BotPuroSqlServer/Logger.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using System.Reflection;
+using System.IO;
+
+
+namespace BotVinculacionUnitec {
+
+    class Logger {
+
+        public static ILog log;
+
+        public static void Log(string message,LogType logType) {
+            EnsureLogger();
+
+            switch (logType) {
+                case LogType.Error:
+                    log.Error(message);
+                    break;
+
+                case LogType.Warn:
+                    log.Warn(message);
+                    break;
+
+                case LogType.Fatal:
+                    log.Fatal(message);
+                    break;
+				
+				case LogType.Info:
+					log.Info(message);
+					break;
+					
+            }
+        }
+
+
+        private static void EnsureLogger() {
+            if (log != null) return;
+            var assembly = Assembly.GetEntryAssembly();
+            var logRepository = LogManager.GetRepository(assembly);
+            var configFile = getConfigFile();
+
+            //Configuracion de log4net
+            XmlConfigurator.Configure(logRepository, configFile);
+            log = LogManager.GetLogger(assembly, assembly.ManifestModule.Name.Replace(".dll", "").Replace(".", " "));
+           
+        }
+
+
+        private static FileInfo getConfigFile() {
+            FileInfo configFile = null;
+            //el archivo esta en la direccion BotPuroSqlServer\bin\Debug\netcoreapp3.1\
+            var ConfigFileNames = new[] { "Config/log4net.config", "log4net.config" };
+            
+            foreach(var configFileNames in ConfigFileNames){
+                configFile = new FileInfo(configFileNames);
+                if (configFile.Exists) break;
+            }
+            if (configFile == null || !configFile.Exists) 
+                    throw new NullReferenceException("Archivo log4net.config no encontrado");
+            return configFile;
+        }
+  
+
+    }
+}
